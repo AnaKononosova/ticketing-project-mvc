6467fa124189669f17ed0d6104548cca9c574fd1
Anastasia <anastasia.kononosova@ncino.com>
2024-03-20 16:47:55 -0600

    part 3

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,13 +40,23 @@ public class UserController
   public String insertUser(@ModelAttribute("user")UserDTO user, //passes User Object from createUser method
       Model model)
   {
-    model.addAttribute("user", new UserDTO());
-    model.addAttribute("roles", roleService.findAll());
-
     userService.save(user); // Object from method parameter
-    model.addAttribute("users", userService.findAll());
-    return "redirect:user/create"; //redirects to the same endpoint
+    return "redirect:/user/create"; //redirects to the same endpoint
   }
 
+  @GetMapping("/update/{username}")
+  public String editUser(@PathVariable("username")String username, Model model)
+  {
+    model.addAttribute("user", userService.findById(username));
+    model.addAttribute("roles", roleService.findAll());
+    model.addAttribute("users", userService.findAll());
+    return "user/update";
+  }
 
+  @PostMapping("/update")
+  public String updateUser(UserDTO user)
+  {
+    userService.update(user);
+    return "redirect:/user/create";
+  }
 }
--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -5,6 +5,4 @@ import com.cydeo.dto.UserDTO;
 public interface UserService extends CrudService<UserDTO, String>
 {
 
-
-
 }
